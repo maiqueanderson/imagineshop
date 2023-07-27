73c186e0f2b39205d316c1547653738f92cd76c4
Máique Anderson <maique.ss@gmail.com>
2023-07-27 17:06:58 -0300

    implementation of footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,73 @@
+import { styled } from "styled-components";
+import Image from "next/image";
+
+import Logo from '../../../public/logo.png'
+import { Container } from "../styles/util";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faFacebookSquare, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () =>{
+    return(
+        <>
+            <StyledFooter>
+                <StyledFooterContainer>
+                    
+                    <Image src={Logo} width={130} height={60} alt="ImagineShop Logo"/>
+                   <StyledFooterContact>
+
+                    Imagine Shop - +55 (48) 3771 - 1703 3771 - 1823 - imagine@imagineschool.com.br - Rua Miguel Daux, 129 - Coqueiros - Florianópolis/SC
+                   </StyledFooterContact>
+
+                    <StyledSocialMediaLinks>
+                        <li>
+                            <StyledSocialMediaIcon icon={faFacebookSquare} />
+                        </li>
+                        <li>
+                        <StyledSocialMediaIcon icon={faInstagram} />
+                        </li>
+                        <li>
+                        <StyledSocialMediaIcon icon={faYoutube}/>
+                        </li>
+                    </StyledSocialMediaLinks>
+                </StyledFooterContainer>
+            </StyledFooter>
+        </>
+    );
+};
+
+const StyledFooter = styled.footer`
+    width: 100vw;
+    height: 12.5rem;
+    background-color: #f4f4f4;
+`;
+
+const StyledFooterContainer = styled.div`
+    ${Container};
+    display: grid;
+    grid-template-columns: 130px auto 130px;
+    padding: 2.5rem 0;
+`;
+
+const StyledFooterContact = styled.p`
+    color: ${({theme}) => theme.colors.secondary};
+    font-size: 0.75rem;
+    margin: 0;
+    text-align: center;
+    margin-top: 8.125rem;
+`;
+
+const StyledSocialMediaLinks = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  gap: 1.5rem;
+  
+`;
+
+const StyledSocialMediaIcon = styled(FontAwesomeIcon)`
+    color: ${({theme}) => theme.colors.secondary};
+    font-size: 1.875rem;
+`;
+
+export default Footer;
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,35 +1,40 @@
 'use client';
 
 import { styled } from "styled-components";
-import Logo from '../../../public/logo.png';
-import ShoppingCart from '../../../public/carrinho.png'
-
 import Image from "next/image";
 import Link from "next/link";
+import Logo from '../../../public/logo.png';
+import ShoppingCartImage from '../../../public/carrinho.png';
+import { Container } from "../styles/util";
 
-const Header = () =>{
-   return(
+
+const Header = ({ text }: any) => {
+
+  return (
     <>
-        <StyledHeader>
-            <StyledNavbar>
-                <Image src={Logo} width={200} height={100} alt="ImagineShop logo"/>
-                <StyleMenu>
-                    <StyleMenuItem>
-                        <Link href='/'>Home</Link>
-                    </StyleMenuItem>
-                    <StyleMenuItem>
-                    <Link href='/about'>Sobre</Link>
-                    </StyleMenuItem>
-                    <StyleMenuItem>
-                    <Link href='/shopping-cart'>
-                        <Image src={ShoppingCart} width={52} height={52} alt="Shopping Cart Image"/>
-                    </Link>
-                    </StyleMenuItem>
-                </StyleMenu>
-            </StyledNavbar>
-        </StyledHeader>
+      <StyledHeader>
+        <StyledNavbar>
+          <Image src={Logo} width={200} height={100} alt="imagineshop logo" />
+
+          <StyledMenu>
+            <StyledMenuItem>
+              <Link href="/">Home</Link>
+            </StyledMenuItem>
+
+            <StyledMenuItem>
+              <Link href="/about">Sobre</Link>
+            </StyledMenuItem>
+
+            <StyledMenuItem>
+              <Link href="/shopping-cart">
+                <Image src={ShoppingCartImage} width={52} height={52} alt="shopping cart image"/>
+              </Link>
+            </StyledMenuItem>
+          </StyledMenu>
+        </StyledNavbar>
+      </StyledHeader>
     </>
-   )
+  )
 }
 
 const StyledHeader = styled.header`
@@ -38,38 +43,33 @@ const StyledHeader = styled.header`
 `;
 
 const StyledNavbar = styled.nav`
-    width: 1140px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin: 0 auto;
-`
+  ${Container};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
-const StyleMenu = styled.ul`
-    list-style-type: none;
-    padding: 0;
-    margin: 0;
-    display: flex;
-    gap: 2.5rem;
-    align-items: center;
+const StyledMenu = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  gap: 2.5rem;
+  align-items: center;
+`;
 
-`
+const StyledMenuItem = styled.li`
+  font-size: 16px;
+  font-weight: 700;
 
-const StyleMenuItem = styled.li`
-    font-size: 16px;
-    font-weight: 700;
+  a {
+    text-decoration: none;
+    color: #000;
+  }
 
-    a{
-        text-decoration: none;
-        color: #000;
-
-    }
-
-    a:hover{
-        color: orange;
-    }
-
-`
+  a:hover {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
 
 export default Header;
-
