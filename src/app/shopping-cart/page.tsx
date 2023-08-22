ae13f3edbac543b255a0f4787912c45b23b4e75a
Máique Anderson <maique.ss@gmail.com>
2023-08-22 11:03:31 -0300

    implementation of shopping cart

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -1,4 +1,15 @@
+'use client';
+import { useContext, useEffect } from "react";
+import { ShoppingCartContext } from "../contexts/ShoppingCart";
+
 const ShoppingCart = () =>{
+    //aqui esta pegando os produtos do context ShoppingCart e trazendo para a página de carrinho
+    const {getProducts} = useContext(ShoppingCartContext);
+    useEffect(() => {
+        const products = getProducts();
+        console.log(products);
+    })
+
     return(
         <>
             <p>Shopping Cart Works</p>
